e3251ba4c3b1dc02059a8d4a58b84fa793570d37
beenarani <ranibeena187@gmail.com>
2024-07-02 20:25:54 +0100

    My Homework Week sw 7

---
+++ src/test/java/com/bank/runner/regressionTest.java
@@ -0,0 +1,17 @@
+package com.bank.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/resources/features", // path of features package
+        glue = "com/bank/steps",  // path of steps package
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",// path of html report   // extent report website
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = "@regression"
+
+)
+public class regressionTest extends AbstractTestNGCucumberTests {
+
+}
---
+++ src/test/java/com/bank/runner/smokeTest.java
@@ -0,0 +1,18 @@
+package com.bank.runner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/resources/features", // path of features package
+        glue = "com/bank/steps",  // path of steps package
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",// path of html report   // extent report website
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = "@smoke"
+
+)
+public class smokeTest extends AbstractTestNGCucumberTests {
+
+}
